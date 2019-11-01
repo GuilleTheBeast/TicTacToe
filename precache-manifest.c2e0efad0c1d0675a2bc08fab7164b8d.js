edd5429b50fe73622d10dc5ea8eab156d7767492
GuilleTheBeast <00026018@uca.edu.sv>
2019-11-01 11:28:49 -0600

    Updates

---
+++ precache-manifest.c2e0efad0c1d0675a2bc08fab7164b8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82731559035b7f2b4417086285074ed0",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "b04d3f19847d0416410e",
+    "url": "/TicTacToe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/TicTacToe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "b04d3f19847d0416410e",
+    "url": "/TicTacToe/static/js/main.e18e462e.chunk.js"
+  },
+  {
+    "revision": "664d87082df4062f2db9",
+    "url": "/TicTacToe/static/js/runtime-main.326ff5c5.js"
+  }
+]);
